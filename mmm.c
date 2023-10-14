4dcab5672a59fb94fbfd2b3f6d91d862f1d2de94
JuanM0412 <juangomezp1204@gmail.com>
2023-10-14 15:13:28 -0500

    Matrix-Matrix Multiplication tester and statistics

--- mmm.c
+++ mmm.c
@@ -1,26 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <cblas.h>
 
 int randomGenerator();
 
 int main(){
     srand((unsigned) time(NULL));
     int size = randomGenerator();
-    printf("%d", size);
-    printf("\n");
-    int A[size][size], B[size][size], C[size][size];
-
+    double timeSpentCheck = 0.0;
+    double timeSpentMMM = 0.0;
+    float A[size][size], B[size][size], C[size][size], checkMatrix[size][size];
+    
     for(int i = 0; i < size; i++){
         for(int j = 0; j < size; j++){
             int n1 = randomGenerator();
             int n2 = randomGenerator();
-            A[i][j] = n1; 
-            B[i][j] = n2; 
+            A[i][j] = (float)n1; 
+            B[i][j] = (float)n2; 
             C[i][j] = 0;
         }
     }
 
+    clock_t beginMMM = clock();
     for(int i = 0; i < size; i++){
         for(int j = 0; j < size; j++){
             for(int k = 0; k < size; k++){
@@ -28,21 +30,39 @@ int main(){
             }
         }
     }
+    clock_t endMMM = clock();
 
+    clock_t beginCheck = clock();
+    cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, size, size, size, 1.0, &A[0][0], size, &B[0][0], size, 0.0, &checkMatrix[0][0], size);
+    clock_t endCheck = clock();
+
+    int flag = 1;
     for(int i = 0; i < size; i++){
         for(int j = 0; j < size; j++){
-            printf("%d", C[i][j]);
-            if (j < size - 1) {
-                printf(" ");
+            if(checkMatrix[i][j] != C[i][j]){
+                printf("Wrong");
+                flag = 0;
+                break;
             }
         }
-        printf("\n");
+        if(flag == 0){
+            break;
+        }
+    }
+
+    timeSpentCheck += (double)(endCheck - beginCheck) / CLOCKS_PER_SEC;
+    timeSpentMMM += (double)(endMMM - beginMMM) / CLOCKS_PER_SEC;
+    if(flag == 1){
+        printf("Size of matrices: %d \n", size);
+        printf("CBLAS function: %f \n", timeSpentCheck);
+        printf("Our function: %f \n", timeSpentMMM);
+        printf("Ratio: %f \n", (timeSpentMMM / timeSpentCheck));
     }
 
     return 0;
 }
 
 int randomGenerator(){
-    int num = 2 + (rand() % 9);
+    int num = 2 + (rand() % 500);
     return num;
 }
